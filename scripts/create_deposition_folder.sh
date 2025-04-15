afcab89b227f8a10d0e19c706b86e133fd53cf34
Sabar Dasgupta <s@bardasgupta.com>
2025-04-15 19:04:57 -0400

    clean up deposition

--- scripts/create_deposition_folder.sh
+++ scripts/create_deposition_folder.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
+set -e
+
 REPO=$1
 RAW_DATA_FOLDER=$2
 DERIV_DATA_FOLDER=$3
 CONTAINER_IMAGE=$4
 OUTPUT_FOLDER=$5
-NAME=watch_calibration.git
+BARE_REPO_NAME="watch-calibration.git"
 
 if [ -d "$OUTPUT_FOLDER" ]; then
     echo "OUTPUT_FOLDER exists. Remove or run with a different OUTPUT_FOLDER."
@@ -12,14 +16,16 @@ fi
 
 git clone $REPO $OUTPUT_FOLDER
 pushd $OUTPUT_FOLDER
-git remote remove origin
+rm -rf .git
 popd
 
 cp $CONTAINER_IMAGE $OUTPUT_FOLDER
 cp -r $RAW_DATA_FOLDER $OUTPUT_FOLDER
 cp -r $DERIV_DATA_FOLDER $OUTPUT_FOLDER
 
-git clone --bare $REPO "$OUTPUT_FOLDER/watch_calibration.git"
-pushd "$OUTPUT_FOLDER/watch_calibration.git"
+git clone --bare --no-local $REPO "$OUTPUT_FOLDER/$BARE_REPO_NAME"
+pushd "$OUTPUT_FOLDER/$BARE_REPO_NAME"
+git gc --aggressive --prune=now
+git repack -adf
 git update-server-info
 popd
